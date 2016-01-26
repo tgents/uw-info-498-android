60758ce68d3a5d0b57b2a6d4fc65f2898753553a
tytseng <thomasytseng@gmail.com>
2016-01-26 13:35:32 -0800

    summary works now

--- selftracker/app/src/main/java/edu/uw/tgents/selftracker/SummaryFragment.java
+++ selftracker/app/src/main/java/edu/uw/tgents/selftracker/SummaryFragment.java
@@ -30,15 +30,24 @@ public class SummaryFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.detail_layout, container, false);
 
         TextView titleView = (TextView) rootView.findViewById(R.id.title);
+        final TextView descView = (TextView) rootView.findViewById(R.id.description);
         titleView.setText("Summary");
 
-        Bundle bundle = getArguments();
-        int count;
-        if(bundle != null){
-            count = bundle.getInt("numAct");
-            TextView descView = (TextView) rootView.findViewById(R.id.description);
-            descView.setText("You currently have " + count + " activities!");
-        }
+        Firebase fireDB = new Firebase("https://infoselftracker.firebaseio.com/activities");
+        fireDB.addValueEventListener(new ValueEventListener() {
+            ArrayList<Activity> tempList;
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int temp = (int) snapshot.getChildrenCount();
+                descView.setText("You have recorded " + temp + " activities!");
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.v("MainActivity", "The read failed: " + firebaseError.getMessage());
+            }
+        });
 
         return rootView;
     }
